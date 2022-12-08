49e07fe4fb7994f48b925924c52416052f1193df
YanginFire <90453727+YanginFire@users.noreply.github.com>
2022-12-08 20:04:43 +0300

    Update AdminMenu.java

--- src/main/java/org/example/task9/AdminMenu.java
+++ src/main/java/org/example/task9/AdminMenu.java
@@ -14,6 +14,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class AdminMenu implements ActionListener {
     /*
@@ -107,10 +108,16 @@ public class AdminMenu implements ActionListener {
             System.out.println("Invalid password");
         }
     }
-
+    /**
+     * Функция для отрисовки интерфейса администратора
+     * <p>
+     * delete and Update func - DONE
+     *
+     * @return
+     */
     private static Component frame_for_editor() {
         JPanel editorpanel = new JPanel();
-        JFrame editorframe = new JFrame();
+        JFrame editorframe = new JFrame("Editor interface");
         JButton updateButton = new JButton();
         JButton delButton = new JButton();
 
@@ -149,20 +156,21 @@ public class AdminMenu implements ActionListener {
     /**
      * Функция для отрисовки интерфейса администратора
      * <p>
-     * ADD and Update func - DONE
-     *
-     * delete - NOT DONE
+     * ADD, delete and Update func - DONE
      *
      * @return
      */
     public static Component frame_for_admin() {
 
         JPanel adminpanel = new JPanel();
-        JFrame adminframe = new JFrame();
+        JFrame adminframe = new JFrame("Administrator interface");
         JButton addButton = new JButton();
         JButton updateButton = new JButton();
 
+
         JButton delete_Button = new JButton();
+
+        JButton display_Button = new JButton();
 
         adminframe.setSize(600,600);
         adminframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -183,23 +191,6 @@ public class AdminMenu implements ActionListener {
 
         addButton = new JButton("Добавить");
         addButton.setBounds(10,320,350,300);
-
-
-        /**
-         * Функция которая спрашивает пользователя здесь ли он?
-         */
-
-//        JOptionPane optionPane = new JOptionPane();
-//
-//        Timer timer = new Timer(15000, e -> {
-//            optionPane.setVisible(false);
-//            optionPane.getRootFrame().dispose();
-//        });
-//        timer.setRepeats(false);
-//        timer.start();
-//
-//
-//        int n = optionPane.showConfirmDialog(null, "Are you here?", null, JOptionPane.YES_NO_OPTION);
 
         addButton.addActionListener(new ActionListener() {
             @Override
@@ -223,16 +214,44 @@ public class AdminMenu implements ActionListener {
             }
         });
         adminpanel.add(updateButton);
+
+
+//        JTable tblData = new JTable();
+//        tblData.setBounds(10,300,350,300);
+//        adminpanel.add(tblData);
+
+        display_Button = new JButton("Display data");
+        display_Button.setBounds(10,300,350,300);
+
+        display_Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Data displayed");
+                display_the_db();
+                display_the_database();
+            }
+        });
+        adminpanel.add(display_Button);
+
+
         return null;
     }
 
+    /**
+     *  Функция поиска по id, поиск по имени, Расчет зарплаты по базе данных, поиск по дате рождения - DONE
+     *
+     * @return
+     */
     public static Component frame_for_viewer(){
         JPanel viewerpanel = new JPanel();
-        JFrame viewerframe = new JFrame();
+        JFrame viewerframe = new JFrame("Viewer interface");
         JButton addButton = new JButton();
         JButton updateButton = new JButton();
 
         JButton find_id_Button = new JButton();
+        JButton find_by_name_Button = new JButton();
+        JButton find_by_birthdate_But = new JButton();
+        JButton sum_of_salary_But = new JButton();
 
         viewerframe.setSize(600,600);
         viewerframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -249,14 +268,61 @@ public class AdminMenu implements ActionListener {
                 find_by_id();
             }
         });
+
+
         viewerpanel.add(find_id_Button);
+
+        find_by_name_Button = new JButton("Найти сотрудника по имени");
+        find_by_name_Button.setBounds(10,320,350,300);
+
+        find_by_name_Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("find by name");
+                find_by_name();
+            }
+        });
+
+        viewerpanel.add(find_by_name_Button);
+
+        find_by_birthdate_But = new JButton("Найти сотрудника по дате рождения");
+        find_by_birthdate_But.setBounds(10,320,350,300);
+
+        find_by_birthdate_But.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("find by name");
+                find_by_birthdate();
+            }
+        });
+
+        viewerpanel.add(find_by_birthdate_But);
+
+        sum_of_salary_But = new JButton("Расчет суммы зарплаты сотрудников");
+        sum_of_salary_But.setBounds(10,320,350,300);
+
+        sum_of_salary_But.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("find by name");
+                sum_of_salary();
+            }
+        });
+
+        viewerpanel.add(sum_of_salary_But);
+
+
+
 
         return null;
     }
 
     /**
      * Сделан функционал
-     * Вывод в отдельное окно или таблицу (????)
+     * | find_by_id
+     * | find_by_name
+     * | find_by_birthdate
+     * Вывод в отдельное окно или таблицу (????) - display_the_db
      */
     public static void find_by_id() {
         JPanel find_by_idpanel = new JPanel();
@@ -293,7 +359,7 @@ public class AdminMenu implements ActionListener {
                 final String USERNAME = "root";
                 final String PASSWORD = "root";
 
-                int id_que = Integer.parseInt(id_quest.getText());
+                String id_que = id_quest.getText();
 
                 String QUERY = "SELECT * FROM people WHERE id=";
 
@@ -328,6 +394,268 @@ public class AdminMenu implements ActionListener {
 
         find_by_idframe.setVisible(true);
 
+    }
+
+    public static void find_by_name() {
+        JPanel find_by_namepanel = new JPanel();
+
+        JFrame find_by_nameframe = new JFrame();
+        find_by_nameframe.setLayout(new BorderLayout());
+        JButton find_by_nameBut = new JButton("Найти");
+        JLabel quest = new JLabel("Введите id сотрудника:");
+
+        JLabel nothing = new JLabel(" ");
+        JTextField name_quest = new JTextField(20);
+
+        find_by_nameframe.setSize(280,150);
+
+        find_by_nameframe.add(find_by_namepanel, BorderLayout.CENTER);
+
+        quest.setBounds(300,600,80,25);
+        find_by_namepanel.add(quest, BorderLayout.CENTER);
+
+        find_by_namepanel.add(nothing, BorderLayout.CENTER);
+
+        name_quest.setBounds(300,540,400,450);
+        find_by_namepanel.add(name_quest, BorderLayout.CENTER);
+
+
+        find_by_nameBut.setBounds(300,0,350,300);
+        find_by_namepanel.add(find_by_nameBut, BorderLayout.CENTER);
+
+        find_by_nameBut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                final String DATABASE_URL = "jdbc:mysql://localhost:3306/mydb";
+                final String USERNAME = "root";
+                final String PASSWORD = "root";
+
+                String firstName = name_quest.getText();
+
+                String QUERY = "SELECT * FROM people WHERE firstname=";
+
+                QUERY += "'" + firstName + "'";
+
+                Connection conn = null;
+                try {
+                    conn = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+                    Statement stmt = conn.createStatement();
+//                    stmt.executeQuery(QUERY);
+                    ResultSet rs = stmt.executeQuery(QUERY); {
+                        System.out.println("Сотрудник найден!");
+                        while (rs.next()) {
+                            System.out.println("=========================== ID: " + rs.getInt("id") + " ==========================");
+                            System.out.println("Имя: " + rs.getString("firstname"));
+                            System.out.println("Фамилия: " + rs.getString("surname"));
+                            System.out.println("Дата Рождения: " + rs.getString("birthdate"));
+                            System.out.println("Место Рождения: " + rs.getString("birthplace"));
+                            System.out.println("Зарплата: " + rs.getString("salary"));
+                            System.out.println("Статус: " + rs.getString("status"));
+                            System.out.println("************************************************************");
+                        }
+
+                    }
+                    System.out.println("Сотрудник найден");
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+            }
+        });
+
+        find_by_nameframe.setVisible(true);
+
+    }
+
+    public static void find_by_birthdate(){
+        JPanel find_by_birthdatepanel = new JPanel();
+
+        JFrame find_by_birthdateframe = new JFrame();
+        find_by_birthdateframe.setLayout(new BorderLayout());
+        JButton find_by_birthdateBut = new JButton("Найти");
+        JLabel quest = new JLabel("Введите дату рождения сотрудника:");
+
+        JLabel nothing = new JLabel(" ");
+        JTextField birthdate_quest = new JTextField(20);
+
+        find_by_birthdateframe.setSize(280,150);
+
+        find_by_birthdateframe.add(find_by_birthdatepanel, BorderLayout.CENTER);
+
+        quest.setBounds(300,600,80,25);
+        find_by_birthdatepanel.add(quest, BorderLayout.CENTER);
+
+        find_by_birthdatepanel.add(nothing, BorderLayout.CENTER);
+
+        birthdate_quest.setBounds(300,540,400,450);
+        find_by_birthdatepanel.add(birthdate_quest, BorderLayout.CENTER);
+
+
+        find_by_birthdateBut.setBounds(300,0,350,300);
+        find_by_birthdatepanel.add(find_by_birthdateBut, BorderLayout.CENTER);
+
+        find_by_birthdateBut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                final String DATABASE_URL = "jdbc:mysql://localhost:3306/mydb";
+                final String USERNAME = "root";
+                final String PASSWORD = "root";
+
+                String birthDate = birthdate_quest.getText();
+
+                String QUERY = "SELECT * FROM people WHERE birthdate=";
+
+                QUERY += "'" + birthDate + "'";
+
+                Connection conn = null;
+                try {
+                    conn = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+                    Statement stmt = conn.createStatement();
+//                    stmt.executeQuery(QUERY);
+                    ResultSet rs = stmt.executeQuery(QUERY); {
+                        System.out.println("Сотрудник найден!");
+                        while (rs.next()) {
+                            System.out.println("=========================== ID: " + rs.getInt("id") + " ==========================");
+                            System.out.println("Имя: " + rs.getString("firstname"));
+                            System.out.println("Фамилия: " + rs.getString("surname"));
+                            System.out.println("Дата Рождения: " + rs.getString("birthdate"));
+                            System.out.println("Место Рождения: " + rs.getString("birthplace"));
+                            System.out.println("Зарплата: " + rs.getString("salary"));
+                            System.out.println("Статус: " + rs.getString("status"));
+                            System.out.println("************************************************************");
+                        }
+
+                    }
+                    System.out.println("Сотрудник найден");
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+            }
+        });
+
+        find_by_birthdateframe.setVisible(true);
+
+
+    }
+
+    /**
+     * Сделано - функционал и вывод в отдельное диалоговое окно
+     */
+    public static void sum_of_salary(){
+
+        double salarySum = 0;
+        String QUERY = "SELECT * FROM people";
+
+        try (
+                Connection conn = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(QUERY);) {
+            while (rs.next()) {
+                salarySum += rs.getDouble("salary");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Произошла ошибка.");
+        }
+        if (salarySum != 0) System.out.println("Cумма зарплат сотрудников: " + salarySum + "руб.\n");
+
+        JOptionPane optionPane = new JOptionPane();
+
+        int n = optionPane.showConfirmDialog(null, "Итоговая зарплата составляет:"+ salarySum, null, JOptionPane.CANCEL_OPTION);
+
+    }
+
+    /**
+     * Display the database into window - NOT DONE
+     *
+     * Проблема - не отображается в отдельном окне таблица!
+     *
+     */
+    public static void display_the_db(){
+
+        JFrame frame = new JFrame("Table");
+
+        frame.setSize(600,600);
+
+        JTable tblData = new JTable();
+        tblData.setBounds(30,40,200,300);
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(DATABASE_URL,USERNAME,PASSWORD);
+            Statement stmt = con.createStatement();
+            String query = "SELECT * from people";
+            ResultSet rs = stmt.executeQuery(query);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            DefaultTableModel model = (DefaultTableModel) tblData.getModel();
+
+            int cols =rsmd.getColumnCount();
+            String[] colName = new String[cols];
+            for(int i=0; i<cols; i++ ){
+                colName[i] = rsmd.getColumnName(i+1);
+//                System.out.println(colName[i]);
+
+            }
+            model.setColumnIdentifiers(colName);
+            String id, firstname, surname, birthdate, birthplace, salary, status;
+            while (rs.next()){
+                id = rs.getString(1);
+                firstname = rs.getString(2);
+                surname = rs.getString(3);
+                birthdate = rs.getString(4);
+                birthplace = rs.getString(5);
+                salary = rs.getString(6);
+                status = rs.getString(7);
+
+                String[] row = {id, firstname, surname, birthdate, birthplace, salary, status};
+//                System.out.println(row);
+                model.addRow(row);
+
+            }
+            stmt.close();
+            con.close();
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        frame.setVisible(true);
+
+    }
+
+    public static void display_the_database(){
+        JFrame frame = new JFrame("Test frame");
+
+        String[] columnNames = {
+                "id",
+                "firstname",
+                "surname",
+                "birthdate",
+                "birthplace",
+                "salary",
+                "status"
+        };
+
+        String[][] data = {
+                {"1","John", "Do", "12.02.99", "Canada", "50000.00", "SINGLE"},
+                {"2","Salim", "Salem", "30.08.97", "Morocco", "65000.00", "MARRIED"},
+                {"3","Yuri", "Puturin", "01.01.76", "Russia", "70000.00", "DIVORCED"},
+                {"4","Juan", "De La Cruiz", "25.11.01", "Mexico", "55000.00", "MARRIED"},
+
+        };
+
+        JTable table = new JTable(data, columnNames);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        frame.getContentPane().add(scrollPane);
+        frame.setPreferredSize(new Dimension(450, 200));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
     /**
